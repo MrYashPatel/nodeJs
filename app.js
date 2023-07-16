a88c0bd9a8da30b0dc5de886013290b138107bfc
Yashkumar Patel <yashkumarpatel11@gmail.com>
2023-07-16 17:56:10 -0400

    MVCSucess

--- app.js
+++ app.js
@@ -1,30 +1,24 @@
-const http = require('http');
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
-const errorController = require('./controllers/error');
 
 const app = express();
 
-
-
-const adminData = require('./routes/admin');
-const shopRouter = require('./routes/shop');
-
-app.set('view engine','ejs');
-app.set('views','views');
-
+const errorsController = require('./controllers/errors.js');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+app.set('view engine', 'ejs');
+
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminRoutes);
-app.use(shopRouter);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use(errorController.get404);
+app.use(errorsController.get404);
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,22 @@
-const express= require('express');
-const path= require('path');
+const express = require('express');
+
+const adminController = require('../controllers/admin');
+
 const router = express.Router();
-const productController = require('../controllers/product');
 
-//GET add product
-router.get('/add-product',productController.getAddProduct);
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-//POST add product
-router.post('/add-product',productController.postAddProduct);
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+
+// /admin/edit-product => GET
+// router.get('/edit-product', adminController.getEditProduct);
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
+
+// /admin/edit-product => POST
+// router.post('/edit-product', adminController.postEditProduct);
 
 module.exports = router;
